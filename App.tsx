78cd4e7e22404ea8045fab310d29243b603edeab
Dgiannaris <mtsgiannaris@hotmail.com>
2020-03-03 22:23:49 +0200

    messing with colors

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 import { decode, encode } from "base-64";
 import { firebaseConfig } from "./config.js";
 import { addUser } from "./services/userService.js";
+
+
+const screenWidth = Math.round(Dimensions.get('screen').width);  
+const screenHeight = Math.round(Dimensions.get('window').height);  
+
+
 
 //GAMW THN GOOGLE KAI TA PUSH THS
 //atob btoa is missing on the last versions
@@ -28,8 +34,64 @@ const db = firebase.firestore();
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Just press it to firebase</Text>
-      <Button onPress={() => addUser(db, "google", 88)} title="re"></Button>
+      <View
+        style={{
+          backgroundColor: "#1565C0",
+          width: screenWidth,
+          height: screenHeight,
+
+        }}
+      >
+
+        <View style={{width:120,height:30,backgroundColor:'red',alignSelf:"center",marginTop:300}}></View>
+        <View>
+          <Text style={{color:'#CE93D8',fontSize:45,fontWeight:'bold',opacity:0.9,position:'absolute',top:screenHeight*0.37,left:40}}>#ButWhy</Text>
+        </View>
+        <View
+          style={{
+            width: screenWidth * 0.9,
+            height: screenWidth * 0.9,
+            backgroundColor: "#1976D2",
+            borderRadius: 300,
+            position: "absolute",
+            zIndex:1,
+            top: -40,
+            left: -50,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 4
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 4.65,
+
+            elevation: 8
+          }}
+        />
+        <View
+          style={{
+            width: screenWidth * 0.4,
+            height: screenWidth * 0.4,
+            backgroundColor: "#1976D2",
+            borderRadius: 300,
+            position: "absolute",
+            zIndex:1,
+            bottom: 30,
+            right: -50,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 4
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 4.65,
+
+            elevation: 6
+          }}
+        />
+
+        {/* <Button onPress={() => addUser(db, "google", 88)} title="re"></Button> */}
+      </View>
     </View>
   );
 }
--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+  apiKey: "AIzaSyDo8awkh8X5MWLq9SzkkXGuss6_qPtRLEk",
+  authDomain: "butwhy-92e98.firebaseapp.com",
+  databaseURL: "https://butwhy-92e98.firebaseio.com",
+  projectId: "butwhy-92e98",
+  storageBucket: "butwhy-92e98.appspot.com",
+  messagingSenderId: "784247029172",
+  appId: "1:784247029172:web:d70db2ee4b6306c9ff86e1",
+  measurementId: "G-C7J3V76TSQ"
   };
